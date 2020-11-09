6acd2425b5bf5dd9a99d62d75bd7e42479bb2105
tom <tom@mdashx.com>
2020-11-09 10:08:20 -0600

    Update example env vars

--- sample.env.sh
+++ sample.env.sh
@@ -1,9 +1,21 @@
+# -*- mode: sh -*-
+
 # Edit this file and save as .env
 # Then load your environment varibles by running:
 # `source .env`
 
 export USER=tom
-export HOST=hydrogen # Name is resolved in .ssh/config
-export WORKING_DIR=webserver
-export CONTAINER_NAME=hydrogen-web
-export EMAIL=name@example.com
+export HOST=hydrogen
+export WORKING_DIR=dhammacharts
+export EMAIL=tom@mdashx.com
+export HOST_DOMAIN=www.dhammacharts.com
+
+
+# Or to create the redirect:
+
+export USER=tom
+export HOST=hydrogen
+export WORKING_DIR=dhammacharts
+export EMAIL=tom@mdashx.com
+export HOST_DOMAIN=dhammacharts.com
+export TARGET=www.dhammacharts.com
